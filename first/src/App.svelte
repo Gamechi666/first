29bc619abf1c38b543eb9848f787049591562c06
Gamechi666 <max.daisuke@ezweb.ne.jp>
2022-04-17 16:48:04 +0900

    ts can now be used

--- first/src/App.svelte
+++ first/src/App.svelte
@@ -1,9 +1,11 @@
-<script>
-	export let name;
+<script lang="ts">
+	import First from "./first.svelte";
+	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
+	<First/>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
 </main>
 
